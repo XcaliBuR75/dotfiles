0b3332192cd36ed46f25895f2a162cc05d166e9c
XcaLiBuR75 <ghernan75@gmail.com>
2022-09-14 03:41:31 -0300

    Added cfg-install

---
+++ cfg-install-improved.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+git clone --bare https://github.com/XcaliBuR75/dotfiles.git $HOME/.cfg
+
+echo ".cfg" >> $HOME/.gitignore
+
+function config {
+    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+
+config checkout
+while [ $? -ge 0 ]
+do
+    config checkout &> /dev/null
+    if [ $? -eq 0 ]
+    then
+        echo "Checked out config."
+        config config status.showUntrackedFiles no
+        break
+    fi
+    echo "Removing up pre-existing dot files."
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} rm -rvf {}
+    config checkout &> /dev/null
+done
+
+#config remote add origin git@github.com:XcaliBuR75/dotfiles.git
